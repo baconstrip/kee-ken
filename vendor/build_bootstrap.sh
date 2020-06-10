f3d20a52a835ec9b1615fcee22f655c305d032ab
Jeff <jwspragg@mtu.edu>
2020-06-10 01:00:18 -0700

    Fix bug in bootstrap build

--- vendor/build_bootstrap.sh
+++ vendor/build_bootstrap.sh
@@ -11,5 +11,8 @@ cd $1
 sudo npm install
 npm run dist
 
+mkdir -p ../../webcontent/static/css/vendor/bootstrap
+mkdir -p ../../webcontent/static/js/vendor/bootstrap
+
 cp -r dist/css/* ../../webcontent/static/css/vendor/bootstrap
 cp -r dist/js/* ../../webcontent/static/js/vendor/bootstrap
